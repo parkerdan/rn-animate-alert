410e95b5ad3aec9d00ff4fc00cc9d6d48c8ba313
parkerdan <parker.daniel.o@gmail.com>
2016-12-03 19:39:23 -0600

    update

--- README.md
+++ README.md
@@ -7,83 +7,86 @@ Gif quality not the greatest, but you get the picture...
 ---
 
 
-![gif](./alert.gif "Alert")
+![gif](./alertNew.gif "Alert")
 
 `npm install --save rn-animate-alert`
 
-- Put this as the bottommost component to overlay all other views.  See Example for how to accomplish this.
+- Put this as the bottommost component to overlay all other views.
+- Wrap whatever content inside
 
-- Will not animate unless you specify what direction.
 
-`springFromTop={true}` or `springFromBottom={true}`
 
 #### Just copy the example and you'll see
 
 ```js
-import React, { Component } from 'react';
-import { View, TouchableOpacity, AppRegistry, Text } from 'react-native';
+import React from 'react';
+import { View, TouchableOpacity, AppRegistry, Text, StyleSheet } from 'react-native';
 import Alert from 'rn-animate-alert';
 
-export default class Example extends Component {
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  button:{
+    borderRadius:5,
+    padding:20,
+    alignItems:'center',
+    backgroundColor:'aqua'
+  },
+  buttonTwo:{
+    borderRadius:5,
+    padding:20,
+    alignItems:'center',
+    backgroundColor:'white'
+  },
+  overlayText:{
+    color:'white',
+    fontWeight:'700'
+  }
+})
+
+export default class Example extends React.Component {
 
   constructor(){
     super();
     this.state={
-      renderAlert: false
+      show: false
     }
   };
 
-
+  toggleAlert = () => {
+    this.setState({show:!this.state.show})
+  }
 
   render(){
     return(
-      <View style={{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
-        backgroundColor:'deeppink'
-      }}>
-        <Text style={{color:'white',fontSize:30, textAlign:'center'}}>I am a React-Native Component</Text>
+      <View style={ styles.container }>
+        <Text>I am a React-Native Component</Text>
         <TouchableOpacity
-          onPress={
-            () => this.setState({renderAlert:true})
-          }
-          style={{
-            borderRadius:5,
-            padding:20,
-            alignItems:'center',
-            backgroundColor:'aqua'
-          }}>
-          <Text>Press Me!</Text>
+          onPress={ this.toggleAlert }
+          style={ styles.button }>
+          <Text>Show Alert</Text>
         </TouchableOpacity>
-        {this.renderAlert()}
+
+
+        <Alert
+        backgroundColor={ 'rgba(111,111,111,.5)' }
+        show={ this.state.show }
+        entrance={ 'fromBottom' }
+        >
+          <View style={ styles.container }>
+            <Text>****I AM AN OVERLAY*****</Text>
+            <TouchableOpacity
+            onPress={ this.toggleAlert }
+            style={ styles.buttonTwo }>
+            <Text>Hide Alert</Text>
+            </TouchableOpacity>
+          </View>
+        </Alert>
       </View>
     )
-  };
-
-  renderAlert(){
-    if (this.state.renderAlert) {
-      return(
-        <Alert
-          title={'I\'m an alert and I animated in!!'}
-          detailText={'Watch me animate out too!!'}
-
-          springFromBottom={true}
-
-          onConfirm={
-            () => {
-              console.log('do stuff');
-              this.setState({renderAlert:false})
-            }
-          }
-          onCancel={
-            () => {
-              console.log('Do other stuff');
-              this.setState({renderAlert:false})
-            }
-          }/>
-      )
-    }
   };
 
 };
@@ -91,25 +94,11 @@ export default class Example extends Component {
 AppRegistry.registerComponent('Example', () => Example);
 ```
 
-#### There's a shit ton of props available to style this thing HOWEVER YOU WANT
 
 ### Props
 
 | Prop | Type | Description | Required |
 | ---  | ---  | ---         | ---      |
-| onConfirm | function | function called when the confirm button is pressed | **YES** |
-| onCancel | function | function called when the cancel button is pressed | **YES** |
-| title | string | text displayed at the top of the alert | **YES** |
-| titleTextStyle | object | styles applied to the title text | no |
-| detailText | string | descriptive text displayed under the title | no |
-| detailTextStyle | object | styles applied to the detail text | no |
-| confirmText | string | text for the confirm button | no |
-| confirmTextStyle | object | styles applied to the confirm text | no |
-| confirmButtonStyle | object | styles applied to the confirm button | no |
-| cancelText | string | text for the cancel button | no |
-| cancelTextStyle | object | styles applied to the cancel text | no |
-| cancelButtonStyle | object | styles applied to the cancel button | no |
-| backgroundColor | string | color applied to the background of the overlay | no |
-| containerStyle | object | styles applied to the container of the text and buttons | no |
-| springFromBottom | boolean | should the alert spring from the bottom | no |
-| spring from top | boolean | should the alert spring from the top | no |
+| entrance | enum oneOf `'fromTop'`,`'fromBottom'`,`'fromLeft'`,`'fromRight'` | how to enter | **YES** |
+| backgroundColor | string | backgroundColor of overlay | **YES**|
+| show | bool | whether to show this | **YES** |
--- index.js
+++ index.js
@@ -1,195 +1,111 @@
 'use strict'
 
-import React, { Component, PropTypes } from 'react';
-import {
-View,
-Dimensions,
-Text,
-TouchableOpacity,
-Animated,
-Easing
-} from 'react-native';
-
+import React from 'react';
+import { View, Dimensions, Animated, StyleSheet } from 'react-native';
 const { height,width } = Dimensions.get('window');
+const styles = StyleSheet.create({container:{position:'absolute',top:0,left:0,right:0,bottom:0}})
 
-const propTypes = {
-onConfirm: React.PropTypes.func.isRequired,
-onCancel: React.PropTypes.func.isRequired,
+export default class Alert extends React.Component {
 
-title: React.PropTypes.string.isRequired,
-titleTextStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
+ static propTypes = {
+    entrance: React.PropTypes.oneOf(['fromTop','fromBottom','fromLeft','fromRight']).isRequired,
+    backgroundColor: React.PropTypes.string.isRequired,
+    show: React.PropTypes.bool.isRequired
+  }
 
-detailText: React.PropTypes.string,
-detailTextStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
-
-confirmText: React.PropTypes.string,
-confirmTextStyle:React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
-confirmButtonStyle:React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
-
-cancelText: React.PropTypes.string,
-cancelTextStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
-cancelButtonStyle:React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
-
-backgroundColor: React.PropTypes.string,
-
-containerStyle: React.PropTypes.oneOfType([React.PropTypes.number,React.PropTypes.object,React.PropTypes.array]),
-
-springFromBottom: React.PropTypes.bool,
-springFromTop: React.PropTypes.bool,
-
-zIndex: React.PropTypes.number, // try and avoid this.  only needed if you used zIndex elsewhere
-
+constructor(props){
+  super(props);
+  this.state = {
+    offset: new Animated.Value(0),
+    show:props.show
+  };
 };
 
-export default class Alert extends Component {
+componentDidMount(){
+  if (this.state.show) {
+    this.animateIn()
+  }
+}
 
- constructor(props){
-   super(props);
-   this.dismiss = this.dismiss.bind(this);
+componentWillReceiveProps(nextProps){
+  if (nextProps.show !== this.state.show) {
+    if (nextProps.show) {
+      this.animateIn()
+    } else {
+      this.animateOut()
+    }
+  }
+}
 
-   this.state = {
-   };
- };
+animateIn = () => {
+  Animated.spring(this.state.offset,{
+    toValue: 1
+  }).start(() => this.setState({show:true}))
+}
 
- componentWillMount(){
-   let xOffset = new Animated.Value(0);
-   let yOffset = new Animated.Value(0);
+animateOut = () => {
+  Animated.spring(this.state.offset,{
+    toValue: 0
+  }).start(() => this.setState({show:false}))
+}
 
+calculateTranslate = () => {
+  switch (this.props.entrance) {
+    case 'fromBottom':
+      return {
+        transform: [{
+          translateY: this.state.offset.interpolate({
+            inputRange: [0,1],
+            outputRange: [height,0]
+          })
+        }]
+      }
+      break;
+    case 'fromTop':
+      return {
+        transform: [{
+          translateY: this.state.offset.interpolate({
+            inputRange: [0,1],
+            outputRange: [-height,0]
+          })
+        }]
+      }
+      break;
 
-   if (this.props.springFromBottom) {
-     yOffset.setValue(1)
-   }
-   if (this.props.springFromTop) {
-     yOffset.setValue(-1)
-   }
+    case 'fromLeft':
+      return {
+        transform: [{
+          translateX: this.state.offset.interpolate({
+            inputRange: [0,1],
+            outputRange: [-width,0]
+          })
+        }]
+      }
+      break;
 
-   this.setState({xOffset:xOffset,yOffset:yOffset})
- };
+    case 'fromRight':
+      return {
+        transform: [{
+          translateX: this.state.offset.interpolate({
+            inputRange: [0,1],
+            outputRange: [width,0]
+          })
+        }]
+      }
+      break;
 
- componentDidMount(){
-   Animated.parallel([
-     Animated.spring(this.state.yOffset,{
-       toValue:0
-     })
-   ]).start()
- };
+    default:
 
- dismiss(cb){
-   let toValue = 0;
-   if (this.props.springFromBottom) {
-     toValue = 1;
-   }
-   if (this.props.springFromTop) {
-     toValue = -1;
-   }
-   Animated.timing(this.state.yOffset,{
-     toValue: toValue,
-     duration: 200,
-     easing: Easing.linear
-   }).start(cb)
- };
+  }
+}
 
- renderText(){
-   if (this.props.detailText) {
-     return(
-       <Text style={[{
-         marginBottom:10
-       },this.props.detailTextStyle]}>
-         {this.props.detailText}
-       </Text>
-     )
-   }
- };
-
- render(){
-   return(
-     <View style={{
-       zIndex:this.props.zIndex || 0,
-       position:'absolute',
-       top:0,
-       left:0,
-       height:height,
-       width:width,
-       backgroundColor: this.props.backgroundColor || 'rgba(111,111,111,.9)',
-       alignItems:'center',
-       justifyContent:'center'
-     }}>
-       <Animated.View style={[{
-         backgroundColor: 'white',
-         borderRadius:5,
-         padding:10
-       },this.props.containerStyle,{
-         transform:[{
-           translateY:this.state.yOffset.interpolate({
-             inputRange: [-1,0,1],
-             outputRange: [-500,0,500]
-           })}]}]}>
-
-         <Text style={[{
-           fontSize:20,
-           fontWeight:'500',
-           marginBottom:10,
-         },this.props.titleTextStyle]}>
-           {this.props.title}
-         </Text>
-
-         {this.renderText()}
-
-         <View style={{
-           flexDirection: 'row'
-         }}>
-           <View style={{
-             flex:1,
-             paddingRight:10
-           }}>
-             <TouchableOpacity
-               style={[{
-                 borderRadius:5,
-                 padding:5,
-                 backgroundColor:'red',
-                 alignItems:'center'
-               },this.props.cancelButtonStyle]}
-               onPress={
-                 () => this.dismiss(this.props.onCancel)
-               }>
-               <Text style={[{
-                 color:'white',
-                 fontWeight:'700'
-               },this.props.cancelTextStyle]}>
-                 {(this.props.cancelText) ? this.props.cancelText:'Cancel'}
-               </Text>
-             </TouchableOpacity>
-           </View>
-
-           <View style={{
-             flex:1,
-             paddingLeft:10
-           }}>
-             <TouchableOpacity
-               style={[{
-                 borderRadius:5,
-                 padding:5,
-                 alignItems:'center',
-                 backgroundColor:'blue',
-               },this.props.confirmButtonStyle]}
-               onPress={
-                 () => this.dismiss(this.props.onConfirm)
-               }>
-               <Text style={[{
-                 color:'white',
-                 fontWeight:'700'
-               },this.props.confirmTextStyle]}>
-                 {(this.props.confirmText) ? this.props.confirmText:'Ok'}
-               </Text>
-             </TouchableOpacity>
-           </View>
-
-         </View>
-       </Animated.View>
-     </View>
-   )
- };
-
+render(){
+  let { backgroundColor:b } = this.props
+  return(
+    <Animated.View style={[ styles.container,{backgroundColor:b},this.calculateTranslate() ]}>
+      { this.props.children }
+    </Animated.View>
+  )
 };
 
-Alert.propTypes = propTypes;
+};
